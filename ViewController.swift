65f9c99dc7d2a62e33efbda4a593c2f7489f3bc6
RedFrame <58191943+TheCode-Jedi@users.noreply.github.com>
2020-02-17 20:33:57 -0500

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Jean Arroyo on 2/13/20.
+//  Copyright © 2020 Jean Arroyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
